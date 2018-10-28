b73e227f85ddc6232a299c935fb39e6a22649096
Kang HyunWoo <khw7147@kaist.ac.kr>
2018-10-29 00:58:34 +0900

    Add Jsoup library

--- src/main/java/services/xis/kadaejeon/Main.java
+++ src/main/java/services/xis/kadaejeon/Main.java
@@ -6,8 +6,13 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
+
 class Main {
-    static String url = "https://talkyou.in/pages/KaDaejeon/posts/15577/";
+    static String url = "https://talkyou.in/pages/KaDaejeon/posts/";
 	static String cookie = "_ga=GA1.2.1295979066.1540732371; _gid=GA1.2.894918707.1540732371; csrftoken=njSmcPEycY44hHMIqb1kkwEghfJNgqiaU230y5cXygpUI6qLmbNDJtsFHCkEmrDZ; sessionid=lt4v8bqjzwk4oi49vensuymkdrph8np1; _gat_gtag_UA_116575140_1=1";
 
     public static void getConnection () {
@@ -24,10 +29,17 @@ class Main {
             StringBuffer response = new StringBuffer();
 
             while ((inputLine = in.readLine()) != null) {
-                System.out.println(inputLine);
+				response.append(inputLine);
             }
             in.close();
 
+			Document doc = Jsoup.parse(response.toString());
+			//Element body = doc.body();
+			//System.out.println(body.text());
+			Elements rows = doc.select("table");
+			for (Element e : rows) {
+				System.out.println(e.text());
+			}
             System.out.println("response code : " + responseCode);
         }
         catch (Exception e) {
