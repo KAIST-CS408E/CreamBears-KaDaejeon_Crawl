a999af3dbe08ad5f1bd2a611ea7263c9644b9047
Kang HyunWoo <khw7147@kaist.ac.kr>
2018-10-29 11:42:21 +0900

    Find a max index among posts

--- src/main/java/services/xis/kadaejeon/Main.java
+++ src/main/java/services/xis/kadaejeon/Main.java
@@ -14,8 +14,13 @@ import org.jsoup.nodes.Element;
 class Main {
     static String url = "https://talkyou.in/pages/KaDaejeon/posts/";
 	static String cookie = "_ga=GA1.2.1295979066.1540732371; _gid=GA1.2.894918707.1540732371; csrftoken=njSmcPEycY44hHMIqb1kkwEghfJNgqiaU230y5cXygpUI6qLmbNDJtsFHCkEmrDZ; sessionid=lt4v8bqjzwk4oi49vensuymkdrph8np1; _gat_gtag_UA_116575140_1=1";
+	static int max_index = 0;
 
-    public static void getConnection () {
+	public static void getPageContent (int index) {
+
+	}
+
+    public static void getMaxPageIndex () {
         try {
             URL obj = new URL(url);
 	        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -34,13 +39,21 @@ class Main {
             in.close();
 
 			Document doc = Jsoup.parse(response.toString());
-			//Element body = doc.body();
-			//System.out.println(body.text());
-			Elements rows = doc.select("table");
-			for (Element e : rows) {
-				System.out.println(e.text());
+			Elements table = doc.select("table");
+
+			for (Element row : table.select("tr")) {
+				String href = row.attr("data-href");
+				String[] href_elems = href.split("/");
+				if (href_elems.length == 5) {
+					int page_index = Integer.parseInt(href_elems[4]);
+					if (max_index < page_index) {
+						max_index = page_index;
+					}
+				}
 			}
             System.out.println("response code : " + responseCode);
+			System.out.println("max_index : " + max_index);
+			con.disclose();
         }
         catch (Exception e) {
             System.out.println("Excpetion : " + e);
